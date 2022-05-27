3b46aeea2de3ff8ceff6e4a8993b0f40cba38183
Caleb Hsu <iamcalebhsu@gmail.com>
2022-05-26 22:18:29 -0700

    Changed a lot we just need to change button layout and randomize order

--- TriviaButton.java
+++ TriviaButton.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
@@ -8,9 +11,14 @@ import java.awt.*;
 public class TriviaButton extends JPanel implements ActionListener {
     private int rand;
     private String question;
-    private String answer;
+    private String answers;
     private String correctAnswer;
-    
+    JButton b1;
+    JButton b2;
+    JButton b3;
+    JButton b4;
+    ArrayList<JButton> randomizedButtonList = new ArrayList<JButton>();
+
     TriviaButton() {
         this.setPreferredSize(new Dimension(GamePanel.SCREEN_WIDTH,GamePanel.SCREEN_HEIGHT));
 		this.setBackground(new Color (142, 30, 76));
@@ -22,23 +30,59 @@ public class TriviaButton extends JPanel implements ActionListener {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        //draw String quesztion prompt
+        
         //add buttons 1 -4 
+        
         //logic written below         
 
+    }
+
+    public void buttonAdd(){
+        buttonRandomize();
+        for (int i = 0; i<randomizedButtonList.size(); i++){
+            this.add(randomizedButtonList.get(i));
+        }
+    }
+
+    public void buttonRandomize(){
+        ArrayList<JButton> list = new ArrayList<JButton>();
+        list.add(b1); list.add(b2); list.add(b3); list.add(b4);
+
+        randomizedButtonList.add(b1);
+        randomizedButtonList.add(b2);
+        randomizedButtonList.add(b3);
+        randomizedButtonList.add(b4);
+        //randomize order into new list for later access
+
+    }
+
+    public void initilizeButtons(String answers){
+        Scanner scan = new Scanner(answers);  
+        scan.useDelimiter(",");  
+
+        b1  = new JButton(scan.next());
+        b2  = new JButton(scan.next());
+        b3  = new JButton(scan.next());
+        b4  = new JButton(scan.next());
+
+        scan.close();  
     }
 
     public void activate() throws FileNotFoundException{
         rand = (int)(Math.random()*(numOfLines("Questions.txt")-1)+1);
         question = getFileLine("Questions.txt", rand);
-        answer = getFileLine("Answers.txt",rand);
+        answers = getFileLine("Answers.txt",rand);
         correctAnswer = getFileLine("CorrectAnswers.txt",rand);
 
-        System.out.println(rand + ": Number chosen with random");
-        System.out.println(question);
+        //System.out.println(rand + ": Number chosen with random");
+        //System.out.println(question);
+        //System.out.println(answer);
+        //System.out.println("cA: " + correctAnswer);
 
-        System.out.println(answer);
-        System.out.println("cA: " + correctAnswer);
+        //buttonAdd initiates other 2 methods above
+        //must be used after buttons have been created using Scanner delimineter
+        initilizeButtons(answers);
+        buttonAdd();
         
         repaint();
     }
@@ -52,7 +96,6 @@ public class TriviaButton extends JPanel implements ActionListener {
         g.drawString(question, 0, 80);
 	}
 
-
     //returns specificec line in a file as a string (later use with scanner del)
     public static String getFileLine(String fileName, int lineNum) {
         String resultString = "";
@@ -63,12 +106,10 @@ public class TriviaButton extends JPanel implements ActionListener {
 			}
 	
 		} catch (FileNotFoundException e) {
-			System.out.println("broken");
+			System.out.println("broken");  //I like this file exception 
 		}
         return resultString;
 	}
-
-
 
     //returns num of lines in a file..  
     public static int numOfLines(String fileName) throws FileNotFoundException {

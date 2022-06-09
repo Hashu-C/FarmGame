eb4382d24bac3e8be719e5169f1348f0c8447323
lauren <56701372+lauren840@users.noreply.github.com>
2022-06-09 00:03:46 -0700

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -18,7 +18,6 @@ public class Plant {
         int rColor;
         int gColor;
         int bColor;
-        int alphaVal;
     int placePrice;
     int maxprogress;
     //endregion
@@ -29,7 +28,7 @@ public class Plant {
     int marketValue;
 
 
-    public Plant(String name,int x, int y, int width, int length, int rColor, int gColor, int bColor, int alphaVal, int placePrice, int harvestPrice,int maxprogress){
+    public Plant(String name,int x, int y, int width, int length, int rColor, int gColor, int bColor, int placePrice, int harvestPrice,int maxprogress){
         this.name = name;
         this.xCord = x;
         this.yCord=y;
@@ -38,13 +37,11 @@ public class Plant {
         this.rColor=rColor;
         this.gColor=gColor;
         this.bColor=bColor;
-        this.alphaVal=alphaVal;
         this.placePrice = placePrice;
         this.maxprogress = maxprogress;
         this.progress = 1;
         this.readyToHarvest = false;
         this.harvestPrice = harvestPrice;
-        
     }
 
 
@@ -55,14 +52,10 @@ public class Plant {
         readyToHarvest=false;
 
     }
-    
-
-
 
     //make a update progress upon trivia completion that checks if progress>=3 then updates readyToHarvest
     public void updateProgress(){
         if (progress+1>maxprogress) {
-
         } else if (progress+1==maxprogress){
             progress++;
             readyToHarvest=true;
@@ -77,11 +70,8 @@ public class Plant {
             progress=1;
             readyToHarvest=false;
         }
-
         GameRunner.frame.money.setText("Money: " + Integer.toString(GameRunner.frame.moneyCount));
     }
-
-
 
     public static int getPlantInUse(){
         return plantInUse;
@@ -91,7 +81,6 @@ public class Plant {
 		plantInUse = plant;
 	}
 
-
     public boolean plantingCost() {
         if (GameRunner.frame.moneyCount>=placePrice){
             return true;
@@ -100,11 +89,10 @@ public class Plant {
         }
     }
     
-
     //returns market value only used in one instance 
     public static int getPlantMarketPrice(int rarity) {
         if (rarity == 0) {
-            
+   
         }
         if (rarity == 1) {
             
@@ -131,249 +119,28 @@ public class Plant {
     }
 
     public static Plant getPlant(int Hori, int Vert) {
-        Plant temp = new Plant("grass",Hori,Vert,3, 3, 0, 200, 0, 240, 5, 1,3 );
-		if(Plant.getPlantInUse()==0){
-             temp =new Plant("grass",Hori,Vert,3, 3, 0, 200, 0, 240, 5,5, 3);
-        }
+        Plant temp = new Plant("grass", Hori, Vert, 3, 3, 140,170,240, 5, 1,3 );
         if(Plant.getPlantInUse()==1){
-             temp =new Plant("rice",Hori,Vert,3, 3, 0, 0, 200, 240, 5,5, 3);
+             temp =new Plant("rice", Hori, Vert,3, 3, 210, 205, 150, 5, 5, 3);
         }
         if(Plant.getPlantInUse()==2){
-             temp =new Plant("sweet potato",Hori,Vert,3, 3, 0, 200, 0, 240, 5,5, 3);
+             temp =new Plant("sweet potato",Hori,Vert,3, 3, 192,158,208, 5,5, 3);
         }
         if(Plant.getPlantInUse()==3){
-             temp =new Plant("cabbage",Hori,Vert,3, 3, 0, 200, 0, 240, 5,5, 3);
+             temp =new Plant("cabbage",Hori,Vert,3, 3, 170,208,150, 5,5, 3);
         }
         if(Plant.getPlantInUse()==4){
-             temp =new Plant("edamme",Hori,Vert,3, 3, 0, 200, 0, 240, 5, 5,3);
+             temp =new Plant("edamme",Hori,Vert,3, 3, 112,195,113, 5, 5,3);
         }
         if(Plant.getPlantInUse()==5){
-             temp =new Plant("papaya",Hori,Vert,3, 3, 0, 200, 0, 240, 5,5, 3);
+             temp =new Plant("papaya",Hori,Vert,3, 3, 235,175,90, 5,5, 3);
         }
         if(Plant.getPlantInUse()==6){
-             temp =new Plant("nectarines",Hori,Vert,3, 3, 0, 200, 0, 240, 5, 5,3);
+             temp =new Plant("nectarines",Hori,Vert,3, 3, 240,175,215, 5, 5,3);
         }
         if (Plant.getPlantInUse() == 7){
-            temp = new Plant("mango",Hori,Vert,3,3,0,0,0,240,5,5,3);
+            temp = new Plant("mango",Hori,Vert,3,3,237,237,130,5,5,3);
         }
-        
-         return temp;
+        return temp;
     }
-
-
-
-    /** 
-    //region
-    //all possible plant values
-    Plant zero = new Plant(3, 3, 0, 200, 0, 240, 5, 3, 0);
-    Plant one = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress, 1);
-    Plant two = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress, 2);
-    Plant three = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress, 3);
-    Plant four = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress,4 );
-    Plant five = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress,5);
-    Plant six = new Plant(width, length, rColor, gColor, bColor, alphaVal, sellPrice, maxprogress,6);
-
-    public int getWidth(){
-        if(getPlantInUse()==0){
-            return zero.width;
-        }
-        if(getPlantInUse()==1){
-            return one.width;
-        }
-        if(getPlantInUse()==2){
-            return two.width;
-        }
-        if(getPlantInUse()==3){
-            return three.width;
-        }
-        if(getPlantInUse()==4){
-            return four.width;
-        }
-        if(getPlantInUse()==5){
-            return five.width;
-        }
-        if(getPlantInUse()==6){
-            return six.width;
-        }
-
-        return 0;
-    }
-    public int getLength(){
-        if(getPlantInUse()==0){
-            return zero.length;
-        }
-        if(getPlantInUse()==1){
-            return one.length;
-        }
-        if(getPlantInUse()==2){
-            return two.length;
-        }
-        if(getPlantInUse()==3){
-            return three.length;
-        }
-        if(getPlantInUse()==4){
-            return four.length;
-        }
-        if(getPlantInUse()==5){
-            return five.length;
-        }
-        if(getPlantInUse()==6){
-            return six.length;
-        }
-
-        return 0;
-    }
-    public int getRColor(){
-        if(getPlantInUse()==0){
-            return zero.rColor;
-        }
-        if(getPlantInUse()==1){
-            return one.rColor;
-        }
-        if(getPlantInUse()==2){
-            return two.rColor;
-        }
-        if(getPlantInUse()==3){
-            return three.rColor;
-        }
-        if(getPlantInUse()==4){
-            return four.rColor;
-        }
-        if(getPlantInUse()==5){
-            return five.rColor;
-        }
-        if(getPlantInUse()==6){
-            return six.rColor;
-        }
-
-        return 0;
-    }
-    public int getGColor(){
-        if(getPlantInUse()==0){
-            return zero.gColor;
-        }
-        if(getPlantInUse()==1){
-            return one.gColor;
-        }
-        if(getPlantInUse()==2){
-            return two.gColor;
-        }
-        if(getPlantInUse()==3){
-            return three.gColor;
-        }
-        if(getPlantInUse()==4){
-            return four.gColor;
-        }
-        if(getPlantInUse()==5){
-            return five.gColor;
-        }
-        if(getPlantInUse()==6){
-            return six.gColor;
-        }
-
-        return 0;
-    }
-    public int getBColor(){
-        if(getPlantInUse()==0){
-            return zero.bColor;
-        }
-        if(getPlantInUse()==1){
-            return one.bColor;
-        }
-        if(getPlantInUse()==2){
-            return two.bColor;
-        }
-        if(getPlantInUse()==3){
-            return three.bColor;
-        }
-        if(getPlantInUse()==4){
-            return four.bColor;
-        }
-        if(getPlantInUse()==5){
-            return five.bColor;
-        }
-        if(getPlantInUse()==6){
-            return six.bColor;
-        }
-
-        return 0;
-    }
-    public int getAlphaVal(){
-        if(getPlantInUse()==0){
-            return zero.alphaVal;
-        }
-        if(getPlantInUse()==1){
-            return one.alphaVal;
-        }
-        if(getPlantInUse()==2){
-            return two.alphaVal;
-        }
-        if(getPlantInUse()==3){
-            return three.alphaVal;
-        }
-        if(getPlantInUse()==4){
-            return four.alphaVal;
-        }
-        if(getPlantInUse()==5){
-            return five.alphaVal;
-        }
-        if(getPlantInUse()==6){
-            return six.alphaVal;
-        }
-
-        return 0;
-    }
-    public int getSellPrice(){
-        if(getPlantInUse()==0){
-            return zero.sellPrice;
-        }
-        if(getPlantInUse()==1){
-            return one.sellPrice;
-        }
-        if(getPlantInUse()==2){
-            return two.sellPrice;
-        }
-        if(getPlantInUse()==3){
-            return three.sellPrice;
-        }
-        if(getPlantInUse()==4){
-            return four.sellPrice;
-        }
-        if(getPlantInUse()==5){
-            return five.sellPrice;
-        }
-        if(getPlantInUse()==6){
-            return six.sellPrice;
-        }
-
-        return 0;
-    }
-    public int getMaxProgress(){
-
-        if(getPlantInUse()==0){
-            return zero.maxprogress;
-        }
-        if(getPlantInUse()==1){
-            return one.maxprogress;
-        }
-        if(getPlantInUse()==2){
-            return two.maxprogress;
-        }
-        if(getPlantInUse()==3){
-            return three.maxprogress;
-        }
-        if(getPlantInUse()==4){
-            return four.maxprogress;
-        }
-        if(getPlantInUse()==5){
-            return five.maxprogress;
-        }
-        if(getPlantInUse()==6){
-            return six.maxprogress;
-        }
-
-        return 0;
-    }
-    //endregion
-    */
 }
